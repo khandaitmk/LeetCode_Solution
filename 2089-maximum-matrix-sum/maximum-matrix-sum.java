75b9ad72e70e725172c21c955e6d32546142f73e
Manish <162733630+khandaitmk@users.noreply.github.com>
2026-01-05 22:55:07 +0530

    Time: 6 ms (73.94%) | Memory: 68 MB (52.73%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -1,42 +1,27 @@
 class Solution {
     public static long maxMatrixSum(int[][] matrix) {
+
         long sum = 0;
-        long small = Integer.MAX_VALUE;
-        ArrayList<Integer> list = new ArrayList<>();
+        int minAbs = Integer.MAX_VALUE;
+        int negCount = 0;
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[i].length;j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
 
-                if(matrix[i][j] < 1){
-                    list.add(matrix[i][j]);
-                }else{
-                    if(matrix[i][j]<small){
-                        small = matrix[i][j];
-                    }
-                    sum +=matrix[i][j];
-                }
+                int val = matrix[i][j];
+
+                if (val < 0) negCount++;
+
+                int absVal = Math.abs(val);
+                sum += absVal;
+                minAbs = Math.min(minAbs, absVal);
             }
         }
-        Collections.sort(list);
-        int n = list.size();
-        long temp = 0;
-        long list_small = Integer.MIN_VALUE;
 
-        for(int i=0;i<list.size();i++){
-            if(list_small<list.get(i)){
-                list_small = list.get(i);
-            }
-            temp +=list.get(i);
+        if (negCount % 2 == 0) {
+            return sum;
         }
-        if(n%2==0){
-            return  sum + ((-1)*temp);
-        }
-            list_small = list_small*(-1);
-            long abc = Math.min(list_small,small);
-            return  sum + ((-1)*temp) - 2*abc;
-        
 
-
-//        return (long)sum;
+        return sum - 2L * minAbs;
     }
 }
