26ec42ef2a452b26d670d7046504c0c92188ec3c
Manish <162733630+khandaitmk@users.noreply.github.com>
2026-01-27 19:53:23 +0530

    Time: 1 ms (81.58%) | Memory: 44.1 MB (5.35%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,53 +15,79 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-       List<Integer> a = new ArrayList<>();
-        List<List<Integer>> a2 = new ArrayList<>();
+    //    List<Integer> a = new ArrayList<>();
+    //     List<List<Integer>> a2 = new ArrayList<>();
 
+    //     if(root == null){
+    //         return a2;
+    //     }
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.add(root);
+    //     q.add(null);
+
+    //     while(!q.isEmpty()){
+    //         TreeNode curr = q.remove();
+    //         if(curr == null){
+    //             if(q.isEmpty()){
+    //                 a2.add(a);
+    //                 break;
+    //             }else{
+    //                 a2.add(a);
+    //                 a = new ArrayList<>();
+    //                 q.add(null);
+    //             }
+    //         }else{
+    //             a.add(curr.val);
+    //             if(curr.left != null){
+    //                 q.add(curr.left);
+    //             }
+    //             if(curr.right != null){
+    //                 q.add(curr.right);
+    //             }
+    //         }
+    //     }
+    //     for(int i = 0; i<a2.size();i++){
+    //         if(i % 2 != 0){
+    //             reverse((ArrayList<Integer>) a2.get(i));
+    //         }
+    //     }
+    //     return a2;
+
+
+    List<List<Integer>> a2 = new ArrayList<>();
         if(root == null){
             return a2;
         }
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        q.add(null);
-
+        boolean leftToRight = true;
         while(!q.isEmpty()){
-            TreeNode curr = q.remove();
-            if(curr == null){
-                if(q.isEmpty()){
-                    a2.add(a);
-                    break;
-                }else{
-                    a2.add(a);
-                    a = new ArrayList<>();
-                    q.add(null);
-                }
-            }else{
-                a.add(curr.val);
-                if(curr.left != null){
-                    q.add(curr.left);
-                }
-                if(curr.right != null){
-                    q.add(curr.right);
-                }
-            }
-        }
-        for(int i = 0; i<a2.size();i++){
-            if(i % 2 != 0){
-                reverse((ArrayList<Integer>) a2.get(i));
-            }
+            int size = q.size();
+            ArrayList<Integer> a = new ArrayList<>();
+                  for(int i = 0 ; i < size ;i++){
+                      TreeNode curr = q.remove();
+                        if(leftToRight){
+                            a.add(curr.val);
+                        }else {
+                            a.add(0,curr.val);
+                        }
+                      if (curr.left != null) q.add(curr.left);
+                      if (curr.right != null) q.add(curr.right);
+                  }
+            a2.add(a);
+            leftToRight = !leftToRight;
         }
         return a2; 
     }
-    static void reverse(ArrayList<Integer> a){
-        int i=0;
-        int j= a.size()-1;
-        while(i<j){
-            int t1 = a.get(i);
-            int t2 = a.get(j);
-            a.set(i++,t2);
-            a.set(j--,t1);
+    // static void reverse(ArrayList<Integer> a){
+    //     int i=0;
+    //     int j= a.size()-1;
+    //     while(i<j){
+    //         int t1 = a.get(i);
+    //         int t2 = a.get(j);
+    //         a.set(i++,t2);
+    //         a.set(j--,t1);
 
-        }
-    }
+    //     }
+    // }
 }
