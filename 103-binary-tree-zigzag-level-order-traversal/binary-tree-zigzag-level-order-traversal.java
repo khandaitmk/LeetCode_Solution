b76e79e108a77e6b847846e9d928e316d82608ea
Manish <162733630+khandaitmk@users.noreply.github.com>
2026-01-27 19:35:01 +0530

    Time: 1 ms (81.58%) | Memory: 43.7 MB (72.30%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+       List<Integer> a = new ArrayList<>();
+        List<List<Integer>> a2 = new ArrayList<>();
+
+        if(root == null){
+            return a2;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            TreeNode curr = q.remove();
+            if(curr == null){
+                if(q.isEmpty()){
+                    a2.add(a);
+                    break;
+                }else{
+                    a2.add(a);
+                    a = new ArrayList<>();
+                    q.add(null);
+                }
+            }else{
+                a.add(curr.val);
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+        }
+        for(int i = 0; i<a2.size();i++){
+            if(i % 2 != 0){
+                reverse((ArrayList<Integer>) a2.get(i));
+            }
+        }
+        return a2; 
+    }
+    static void reverse(ArrayList<Integer> a){
+        int i=0;
+        int j= a.size()-1;
+        while(i<j){
+            int t1 = a.get(i);
+            int t2 = a.get(j);
+            a.set(i++,t2);
+            a.set(j--,t1);
+
+        }
+    }
+}
