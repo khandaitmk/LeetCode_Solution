3b1eef9de2215b054acc1feb6e7e098b57b4fb17
Manish <162733630+khandaitmk@users.noreply.github.com>
2026-03-01 00:16:10 +0530

    Time: 4 ms (80.62%) | Memory: 46.8 MB (63.60%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,23 +10,45 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // int n = lists.length;
+        // for(int i=0;i<n;i++){
+        //     ListNode temp = lists[i];
+        //     while (temp != null){
+        //         pq.add(temp.val);
+        //         temp = temp.next;
+        //     }
+        // }
+        // if(pq.isEmpty()) return null;
+        // ListNode res = new ListNode(pq.remove());
+        // ListNode result = res;
+
+        // while(!pq.isEmpty()){
+        //     res.next = new ListNode(pq.remove());
+        //     res = res.next;
+        // }
+        // return result;
+
+        // optimized // O(k log k)
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
         int n = lists.length;
-        for(int i=0;i<n;i++){
-            ListNode temp = lists[i];
-            while (temp != null){
-                pq.add(temp.val);
-                temp = temp.next;
+        // Add only the HEAD of each list
+        for (ListNode node : lists) {
+            if (node != null) pq.add(node);  
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while (!pq.isEmpty()) {
+            ListNode smallest = pq.remove();   
+            curr.next = smallest;              
+            curr = curr.next;
+
+            if (smallest.next != null) {
+                pq.add(smallest.next);         
             }
         }
-        if(pq.isEmpty()) return null;
-        ListNode res = new ListNode(pq.remove());
-        ListNode result = res;
 
-        while(!pq.isEmpty()){
-            res.next = new ListNode(pq.remove());
-            res = res.next;
-        }
-        return result;
+        return dummy.next;
     }
 }
