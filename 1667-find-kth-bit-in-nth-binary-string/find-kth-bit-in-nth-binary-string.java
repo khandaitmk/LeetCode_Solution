d6080b0159c77dd49d375537dcc7159faa5ca38a
Manish <162733630+khandaitmk@users.noreply.github.com>
2026-03-03 23:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (87.38%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,7 +1,6 @@
 class Solution {
         public char findKthBit(int n, int k) {
-            String res = helper(n);
-            return res.charAt(k-1);
+            return helperOptimized(n,k);
         }
         public static String helper(int n){
             if(n == 1){
@@ -21,5 +20,27 @@ class Solution {
             String inverted = new StringBuilder(new String(arr)).reverse().toString();;
             res = res + "1" + inverted;
             return res;
+    }
+    public static char helperOptimized(int n, int k) {
+        // base case
+        if(n == 1) return '0';
+
+        int mid = (int) Math.pow(2, n-1); // middle index
+
+        if(k == mid){
+            return '1'; // middle is always 1
+        }
+        else if(k < mid){
+            // k is in LEFT half → same as Sn-1
+            return helperOptimized(n-1, k);
+        }
+        else {
+            // k is in RIGHT half → mirror of left half inverted
+            // mirror index in left half
+            int mirrorK = mid - (k - mid);
+            char c = helperOptimized(n-1, mirrorK);
+            // invert the character
+            return c == '0' ? '1' : '0';
+        }
     }
 }
