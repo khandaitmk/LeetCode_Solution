b79ba518511cb77a34b3cfd07d8dfd4afdd2e1ac
Manish <162733630+khandaitmk@users.noreply.github.com>
2026-04-04 22:53:44 +0530

    Time: 3 ms (100.00%) | Memory: 175.6 MB (35.22%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,22 +1,38 @@
 class Solution {
+    // public int[] rearrangeArray(int[] nums) {
+    //     int n = nums.length;
+    //     Queue<Integer> positive = new LinkedList<>();
+    //     Queue<Integer> negative = new LinkedList<>();
+    //     for(int i=0;i<n;i++){
+    //         if(nums[i] > 0){
+    //             positive.add(nums[i]);
+    //         }else{
+    //             negative.add(nums[i]);
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++){
+    //         if(i % 2 == 0){
+    //             nums[i] = positive.remove();
+    //         }else{
+    //             nums[i] = negative.remove();
+    //         }
+    //     }
+    //     return nums;
+    // }
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        Queue<Integer> positive = new LinkedList<>();
-        Queue<Integer> negative = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(nums[i] > 0){
-                positive.add(nums[i]);
+        int[] res = new int[n];
+        int posIndex = 0;
+        int negIndex = 1;
+        for(int num : nums){
+            if(num > 0){
+                res[posIndex] = num;
+                posIndex += 2;
             }else{
-                negative.add(nums[i]);
+                res[negIndex] = num;
+                negIndex += 2;
             }
         }
-        for(int i=0;i<n;i++){
-            if(i % 2 == 0){
-                nums[i] = positive.remove();
-            }else{
-                nums[i] = negative.remove();
-            }
-        }
-        return nums;
+        return res;
     }
 }
