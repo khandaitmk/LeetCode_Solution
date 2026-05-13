f0b176182bf5fe48db828a3e52b4351100a4eacf
Manish <162733630+khandaitmk@users.noreply.github.com>
2026-05-13 15:41:23 +0530

    Time: 1 ms (7.40%) | Memory: 46.4 MB (34.36%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -9,28 +9,60 @@
  * }
  */
 class Solution {
-    public ListNode oddEvenList(ListNode head) {
-        if(head == null){
-            return null;
-        }
-        List<ListNode> l1 = new ArrayList<>();
+    // public ListNode oddEvenList(ListNode head) {
+    //     if(head == null){
+    //         return null;
+    //     }
+    //     List<ListNode> l1 = new ArrayList<>();
 
-        ListNode temp = head;
-        while(temp != null && temp.next !=null ){
-            l1.add(temp.next);
-            ListNode dummy = temp.next;
-            temp.next=temp.next.next;
-            if(temp.next != null){
-                temp = temp.next;
-            }
-            dummy.next=null;       
+    //     ListNode temp = head;
+    //     while(temp != null && temp.next !=null ){
+    //         l1.add(temp.next);
+    //         ListNode dummy = temp.next;
+    //         temp.next=temp.next.next;
+    //         if(temp.next != null){
+    //             temp = temp.next;
+    //         }
+    //         dummy.next=null;       
 
-        }
-        for(int i=0;i<l1.size();i++){
-            temp.next=l1.get(i);
-            temp=temp.next;
-        }
-        return head;
+    //     }
+    //     for(int i=0;i<l1.size();i++){
+    //         temp.next=l1.get(i);
+    //         temp=temp.next;
+    //     }
+    //     return head;
         
+    // }
+    public static ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode odd = null;
+        ListNode even = null;
+        ListNode res = null;
+        ListNode temp_even = null;
+        ListNode temp = head;
+        int i = 1;
+        while(temp != null){
+            if(i % 2 == 0){
+                if(even == null){
+                    even  = new ListNode(temp.val);
+                    temp_even = even;
+                }else{
+                    even.next = new ListNode(temp.val);
+                    even  = even.next;
+                }
+            }else{
+                if(odd == null){
+                    odd = new ListNode(temp.val);
+                    res = odd;
+                }else{
+                    odd.next = new ListNode(temp.val);
+                    odd = odd.next;
+                }
+            }
+            i++;
+            temp = temp.next;
+        }
+        odd.next = temp_even;
+        return res;
     }
 }
